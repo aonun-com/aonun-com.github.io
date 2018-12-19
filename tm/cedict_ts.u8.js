669de3ba688aa7e66f67fd9fa218846649974d55
ddb <ddb1494@gmail.com>
2018-12-19 10:00:04 +0900

    a

--- tm/cedict_ts.u8.js
+++ tm/cedict_ts.u8.js
@@ -1,42 +1,6 @@
-const fs=require('fs');
+const fs = require('fs');
+const path = require('path');
 
-let s=fs.readFileSync('cedict_ts.u8.txt','utf8').trim();
+let text = fs.readFileSync(	'cedict_ts.u8.txt','utf8');
 
-function convertDictionary(str){
-	let res;
-	if(str.length>3&&str.indexOf(' ')){
-		res=str.split('\n').map(e=>{
-			let pair=e.split(' ').filter(e=>e);
-			let s,t;
-			s=pair[0];
-			t=pair[1];
-			return s&&t ? [s,t] :null;
-		}).filter(e=>e);
-	}else{
-		res=[];
-	}
-	return res;
-}
-
-function dictionarySort(arr) {
-	arr.sort((a,b)=>{
-		let al=a[0].length, bl=b[0].length;
-		return al>bl ? -1 : (al<bl? 1 : (a[0]>b[0]?-1:(a[0]<b[0]?1:0)));
-	});
-	return arr;
-}
-
-
-let a=convertDictionary(s);
-console.log(a)
-a=a.map(e=>e.reverse());
-
-dictionarySort(a);
-
-
-// require('fs').writeFileSync('d:/jianfan.txt', JSON.stringify(a,'\n',0))
-
-require('fs').writeFileSync('public.txt', a.map(e=>e.join('\t')).join('\n'));
-
-// console.log(a.slice(0,10));
-
+console.log(text);
